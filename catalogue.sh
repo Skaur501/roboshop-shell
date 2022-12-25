8b58b2d06dde792ec96c9af991b7f27206cf5f1a
Sarbjeet Kaur <sarbjeetkaur@Sarbjeets-MacBook-Pro.local>
2022-12-25 00:25:56 -0500

    catalogue

--- catalogue.sh
+++ catalogue.sh
@@ -10,3 +10,10 @@ unzip -o /tmp/catalogue.zip
 mv catalogue-main catalogue
 cd /home/roboshop/catalogue
 npm install
+
+sed -i -e 's/MONGO_DNSNAME/sarbjeet310.online/' systemd.service
+
+mv /home/roboshop/catalogue/systemd.service /etc/systemd/system/catalogue.service
+systemctl daemon-reload
+systemctl restart catalogue
+systemctl enable catalogue
