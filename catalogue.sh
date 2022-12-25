62d4e153b561c712a9bc6efd1a01e9f6e93e5435
Sarbjeet Kaur <sarbjeetkaur@Sarbjeets-MacBook-Pro.local>
2022-12-25 13:33:47 -0500

    catalogue

--- catalogue.sh
+++ catalogue.sh
@@ -5,8 +5,9 @@ useradd roboshop
 
 curl -s -L -o /tmp/catalogue.zip "https://github.com/roboshop-devops-project/catalogue/archive/main.zip"
 cd /home/roboshop
-sudo rm -rf catalogue
+rm -rf catalogue
 unzip /tmp/catalogue.zip
+rm -rf catalogue-main
 mv catalogue-main catalogue
 cd /home/roboshop/catalogue
 npm install
