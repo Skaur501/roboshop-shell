2b16e08aac75f33295d1cff3b705d8be6e1a17b8
Sarbjeet Kaur <sarbjeetkaur@Sarbjeets-MacBook-Pro.local>
2022-12-26 14:47:20 -0500

    user

--- cart.sh
+++ cart.sh
@@ -5,12 +5,13 @@ useradd roboshop
 
 curl -s -L -o /tmp/cart.zip "https://github.com/roboshop-devops-project/cart/archive/main.zip"
 cd /home/roboshop
+rm -rf cart
 unzip /tmp/cart.zip
 mv cart-main cart
 cd cart
 npm install
 
-sed -i -e 's/REDIS_ENDPOINT/redis.devops69.online/' -e 's/CATALOGUE_ENDPOINT/caralogue.devops69.online/' 
+sed -i -e 's/REDIS_ENDPOINT/redis.devops69.online/' -e 's/CATALOGUE_ENDPOINT/caralogue.devops69.online/' /home/roboshop/cart/systemd.service
 systemctl daemon-reload
 systemctl restart cart
 systemctl enable cart
