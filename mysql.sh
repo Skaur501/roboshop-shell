551ccb09f518940396eee15965a2f81cd0d38804
Sarbjeet Kaur <sarbjeetkaur@Sarbjeets-MacBook-Pro.local>
2022-12-26 17:23:34 -0500

    mysql

--- mysql.sh
+++ mysql.sh
@@ -1,7 +1,12 @@
 curl -s -L -o /etc/yum.repos.d/mysql.repo https://raw.githubusercontent.com/roboshop-devops-project/mysql/main/mysql.repo
-dnf module disable mysql
+dnf module disable mysql -y
 
 yum install mysql-community-server -y
 
 systemctl enable mysqld
 systemctl start mysqld
+
+echo "ALTER USER 'root'@'localhost' IDENTIFIED BY 'Roboshop@1';" > /tmp/root-pass-sql
+DEFAULT_PASSWORD=$(GREP 'A temporary password' /var/log/mysqld |awk '{print $NF}")
+
+cat /tmp/root-pass-sql | mysql --connect-expired-password -uroot -p"${DEFAULT_PASSWORD}"
