b7bef6b566237b37ba8c5220313f7ea87b5970f8
Sarbjeet Kaur <sarbjeetkaur@Sarbjeets-MacBook-Pro.local>
2022-12-26 14:58:07 -0500

    user

--- user.sh
+++ user.sh
@@ -1,17 +1,17 @@
 curl -sL https://rpm.nodesource.com/setup_lts.x | bash
 yum install nodejs -y
-
 useradd roboshop
 
 curl -s -L -o /tmp/user.zip "https://github.com/roboshop-devops-project/user/archive/main.zip"
 cd /home/roboshop
 rm -rf user
-unzip /tmp/user.zip
+unzip -o /tmp/user.zip
 mv user-main user
 cd /home/roboshop/user
 npm install
 
 sed -i -e 's/REDIS_ENDPOINT/redis.devops69.online/' -e 's/MONGO_ENDPOINT/mongo.devops69.online/' systemd.service
+mv /home/roboshop/user/systemd.service /etc/systemd/system/user.service
 systemctl daemon-reload
 systemctl restart user
 systemctl enable user
