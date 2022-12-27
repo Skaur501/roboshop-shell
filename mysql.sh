e55b9564b67ead93b83fe5a3563a5c8d71b0bc0d
Sarbjeet Kaur <sarbjeetkaur@Sarbjeets-MacBook-Pro.local>
2022-12-27 00:31:55 -0500

    mysql

--- mysql.sh
+++ mysql.sh
@@ -10,38 +10,41 @@ fi
 echo -e "\e[32mdisable MYSQL 8 version repo\e[0m"
 #echo disable MYSQL 8 version repo
 dnf module disable mysql -y
-if [ $? -ne 0 ]; then
+if [ $? -eq 0 ]; then
     echo SUCCESS
 else
     echo FAILURE
 fi
 
-echo Install MYSQL
+echo -e "\e[32mInstall MYSQL\e[0m"
+#echo Install MYSQL
 yum install mysql-community-server -y
-if [ $? -ne 0 ] ; then
+if [ $? -eq 0 ] ; then
     echo SUCCESS
 else
     echo FAILURE
 fi
 
-echo enable MYSQL
+echo -e "\e[32menable MYSQL\e[0m"
+#echo enable MYSQL
 systemctl enable mysqld
-if [ $? -ne 0 ]; then
+if [ $? -eq 0 ]; then
     echo SUCCESS
 else
     echo FAILURE
 fi
 
-echo restart MYSQL
+echo -e "\e[32mrestart MYSQL\e[0m"
+#echo restart MYSQL
 systemctl restart mysqld
-if [ $? -ne 0 ]; then
+if [ $? -eq 0 ]; then
     echo SUCCESS
 else
     echo FAILURE
 fi
 
 echo show databases | mysql -uroot -pRoboshop@1
-if [ $? -ne 0 ] # if $? is not equal to 0 , commands will only get executed if condition is true . when our condition is true when Roboshop@1 is not set. suppose if you execute the query and output is 1 that means password is not reset
+if [ $? -eq 0 ] # if $? is not equal to 0 , commands will only get executed if condition is true . when our condition is true when Roboshop@1 is not set. suppose if you execute the query and output is 1 that means password is not reset
 then
 echo "ALTER USER 'root'@'localhost' IDENTIFIED BY 'Roboshop@1';" > /tmp/root-pass-sql
 DEFAULT_PASSWORD=$(grep 'A temporary password' /var/log/mysqld.log | awk '{print $NF}')
