e6b678d2e04827b013a73e167d9f5004b2a947c5
Sarbjeet Kaur <sarbjeetkaur@Sarbjeets-MacBook-Pro.local>
2022-12-27 00:20:11 -0500

    mysql

--- mysql.sh
+++ mysql.sh
@@ -1,10 +1,43 @@
+echo -e "\[33mDownloading MySQL Repo File\e[0m"
+echo Downloading MYSQL Repo File
 curl -s -L -o /etc/yum.repos.d/mysql.repo https://raw.githubusercontent.com/roboshop-devops-project/mysql/main/mysql.repo
+if [ $? -ne 0]; then
+    echo SUCCESS
+else
+    echo FAILURE
+fi
+
+echo disable MYSQL 8 version repo
 dnf module disable mysql -y
+if [ $? -ne 0]; then
+    echo SUCCESS
+else
+    echo FAILURE
+fi
 
+echo Install MYSQL
 yum install mysql-community-server -y
+if [ $? -ne 0]; then
+    echo SUCCESS
+else
+    echo FAILURE
+fi
 
+echo enable MYSQL
 systemctl enable mysqld
-systemctl start mysqld
+if [ $? -ne 0]; then
+    echo SUCCESS
+else
+    echo FAILURE
+fi
+
+echo restart MYSQL
+systemctl restart mysqld
+if [ $? -ne 0]; then
+    echo SUCCESS
+else
+    echo FAILURE
+fi
 
 echo show databases | mysql -uroot -pRoboshop@1
 if [ $? -ne 0 ] # if $? is not equal to 0 , commands will only get executed if condition is true . when our condition is true when Roboshop@1 is not set. suppose if you execute the query and output is 1 that means password is not reset
