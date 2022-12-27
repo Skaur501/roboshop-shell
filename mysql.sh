b81d9375890816a9ae27b4d3ca6f0692692ed72e
Sarbjeet <sarbjeetkaur310@gmail.com>
2022-12-27 17:38:20 -0500

    function

--- mysql.sh
+++ mysql.sh
@@ -42,7 +42,7 @@ systemctl restart mysqld
 STAT $?
 
 echo show databases | mysql -uroot -p${ROBOSHOP_MYSQL_PASSWORD}
-if [ $? -ne 0 ] #if $? is not equal to 0 , commands will only get executed if condition is true . when our condition is true when Roboshop@1 is not set. suppose if you execute the query and output is 1 that means password is not reset
+if [ $? -eq 0 ] #if $? is not equal to 0 , commands will only get executed if condition is true . when our condition is true when Roboshop@1 is not set. suppose if you execute the query and output is 1 that means password is not reset
 then
   echo "ALTER USER 'root'@'localhost' IDENTIFIED BY '${ROBOSHOP_MYSQL_PASSWORD}';" > /tmp/root-pass-sql
   DEFAULT_PASSWORD=$(grep 'A temporary password' /var/log/mysqld.log | awk '{print $NF}')
