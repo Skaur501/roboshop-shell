4b30ad5d31de0e892766ac3f85a3003c4f708108
Sarbjeet <sarbjeetkaur310@gmail.com>
2022-12-27 17:15:43 -0500

    function

--- mysql.sh
+++ mysql.sh
@@ -6,55 +6,40 @@ fi
 
 Roboshop_mysql_password=$1
 
-echo -e "\e[32mDownloading MySQL Repo File\e[0m" # to add color to heading
-#echo Downloading MYSQL Repo File
-curl -s -L -o /etc/yum.repos.d/mysql.repo https://raw.githubusercontent.com/roboshop-devops-project/mysql/main/mysql.repo
-if [ $? -eq 0 ]; then
-    echo SUCCESS
+#this stat function will check if $1 is equal to 1 then its true otherwise its false. $1 means first argument
+STAT() {
+  if [ $1 -eq 0 ]; then
+    eco SUCCESS
 else
     echo FAILURE
     exit
 fi
+}
+
+echo -e "\e[32mDownloading MySQL Repo File\e[0m" # to add color to heading
+#echo Downloading MYSQL Repo File
+curl -s -L -o /etc/yum.repos.d/mysql.repo https://raw.githubusercontent.com/roboshop-devops-project/mysql/main/mysql.repo
+STAT [? #Stat question mark means exit status on the curl command . that stat function will go to the first argument
 
 echo -e "\e[32mdisable MYSQL 8 version repo\e[0m"
 #echo disable MYSQL 8 version repo
 dnf module disable mysql -y
-if [ $? -eq 0 ]; then
-    echo SUCCESS
-else
-    echo FAILURE
-    exit
-fi
+STAT [?
 
 echo -e "\e[32mInstall MYSQL\e[0m" # to add color to heading
 #echo Install MYSQL // to display message
 yum install mysql-community-server -y
-if [ $? -eq 0 ] ; then
-    echo SUCCESS
-else
-    echo FAILURE
-    exit
-fi
+STAT [?
 
 echo -e "\e[32menable MYSQL\e[0m"
 #echo enable MYSQL
 systemctl enable mysqld
-if [ $? -eq 0 ]; then
-    echo SUCCESS
-else
-    echo FAILURE
-    exit
-fi
+STAT [?
 
 echo -e "\e[32mrestart MYSQL\e[0m"
 #echo restart MYSQL
 systemctl restart mysqld
-if [ $? -eq 0 ]; then
-    echo SUCCESS
-else
-    echo FAILURE
-    exit
-fi
+STAT [?
 
 echo show databases | mysql -uroot -p${ROBOSHOP_MYSQL_PASSWORD}
 if [ $? -ne 0 ] #if $? is not equal to 0 , commands will only get executed if condition is true . when our condition is true when Roboshop@1 is not set. suppose if you execute the query and output is 1 that means password is not reset
