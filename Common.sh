20e186741d8c1f6e8ca7e474e15ff0c902526af1
Sarbjeet <sarbjeetkaur310@gmail.com>
2022-12-27 23:50:35 -0500

    common

--- Common.sh
+++ Common.sh
@@ -3,10 +3,16 @@ STAT() {
     echo -e "\e[32mSUCCESS\e[0m"
   else
     echo -e "\e[31mFAILURE\e[0m"
+    echo Check the error in lOG File
  fi
 }
 
 PRINT()
 {
+  echo "-----------$1-------" >>${LOG}
   echo -e "\e[32m$1\e[0m"
 }
+
+LOG=/tmp/COMPONENT.log
+rm -f $LOG
+
--- cart.sh
+++ cart.sh
@@ -1,61 +1,62 @@
+Component=cart
 source Common.sh
 
 PRINT "Download Nodejs Repo"
-curl -sL https://rpm.nodesource.com/setup_lts.x | bash
+curl -sL https://rpm.nodesource.com/setup_lts.x | bash &>>$LOG
 STAT $?
 
 PRINT "install node js"
-yum install nodejs -y
+yum install nodejs -y &>>$LOG
 STAT $?
 
 PRINT "ROBOSHOP USER"
-useradd roboshop
+useradd roboshop &>>$LOG
 STAT $?
 
 PRINT "Download Zip folder"
-curl -s -L -o /tmp/cart.zip "https://github.com/roboshop-devops-project/cart/archive/main.zip"
+curl -s -L -o /tmp/cart.zip "https://github.com/roboshop-devops-project/cart/archive/main.zip" &>>$LOG
 STAT $?
 
 PRINT "GO TO PATH"
-cd /home/roboshop
+cd /home/roboshop &>>$LOG
 STAT $?
 
 PRINT "Remove previous version of app"
-rm -rf cart
+rm -rf cart &>>$LOG
 STAT $?
 
 PRINT "Unzip Folder"
-unzip /tmp/cart.zip
+unzip /tmp/cart.zip &>>$LOG
 STAT $?
 
 PRINT "Rename folder"
-mv cart-main cart
+mv cart-main cart &>>$LOG
 STAT $?
 
 PRINT "Go to Path cart"
-cd cart
+cd cart &>>$LOG
 STAT $?
 
 PRINT "Install NPM"
-npm install
+npm install &>>$LOG
 STAT $?
 
 PRINT "Configure Redis endpoint and catalogue endpoint"
-sed -i -e 's/REDIS_ENDPOINT/redis.devops69.online/' -e 's/CATALOGUE_ENDPOINT/caralogue.devops69.online/' /home/roboshop/cart/systemd.service
+sed -i -e 's/REDIS_ENDPOINT/redis.devops69.online/' -e 's/CATALOGUE_ENDPOINT/caralogue.devops69.online/' /home/roboshop/cart/systemd.service &>>$LOG
 STAT $?
 
 PRINT "Configure systemd file"
-mv /home/roboshop/cart/systemd.service /etc/systemd/system/cart.service
+mv /home/roboshop/cart/systemd.service /etc/systemd/system/cart.service &>>$LOG
 STAT $?
 
 PRINT "Daemon-Reload"
-systemctl daemon-reload
+systemctl daemon-reload &>>$LOG
 STAT $?
 
 PRINT "Restart Cart"
-systemctl restart cart
+systemctl restart cart &>>$LOG
 STAT $?
 
 PRINT "Enable Cart"
-systemctl enable cart
+systemctl enable cart &>>$LOG
 STAT $?
