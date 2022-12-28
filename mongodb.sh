49c047442f31a84b9d7b6f4ef756801b5e86f449
Sarbjeet <sarbjeetkaur310@gmail.com>
2022-12-28 16:34:32 -0500

    functionMYSQL

--- mongodb.sh
+++ mongodb.sh
@@ -1,14 +1,32 @@
-curl -s -o /etc/yum.repos.d/mongodb.repo https://raw.githubusercontent.com/roboshop-devops-project/mongodb/main/mongo.repo
+COMPONENT=mongodb
+source Common.sh
 
-yum install -y mongodb-org
-sed -i -e 's/127.0.0.1/0.0.0.0/' /etc/mongod.conf
+PRINT "Download YUM repo File"
+curl -s -o /etc/yum.repos.d/mongodb.repo https://raw.githubusercontent.com/roboshop-devops-project/mongodb/main/mongo.repo &>>$log
 
-systemctl enable mongod
-systemctl restart mongod
+PRINT "Install Mongodb"
+yum install -y mongodb-org &>>$log
 
-curl -s -L -o /tmp/mongodb.zip "https://github.com/roboshop-devops-project/mongodb/archive/main.zip"
-cd /tmp
-unzip mongodb.zip
-cd mongodb-main
-mongo < catalogue.js
-mongo < users.js
+PRINT "Substitute the value of 127.0.0.1 in config file"
+sed -i -e 's/127.0.0.1/0.0.0.0/' /etc/mongod.conf &>>$log
+
+PRINT "Enable Mongodb service"
+systemctl enable mongod &>>$log
+
+PRINT "Restart Mongodb Service"
+systemctl restart mongod &>>$log
+
+cd $APP_LOC &>>$LOG
+  rm -rf ${CONTENT} &>>$LOG
+
+APP_LOC=/tmp
+CONTENT=mongodb-main
+DOWNLOAD_APP_CODE
+
+cd mongodb-main &>>$LOG
+
+PRINT "Load catalouge schema"
+mongo < catalogue.js &>>$log
+
+PRINT "Load user schema"
+mongo < users.js &>>$log
