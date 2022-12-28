22d55495acc96504dc2f1a6157a44dc78f2406c7
Sarbjeet <sarbjeetkaur310@gmail.com>
2022-12-28 16:39:54 -0500

    functionMYSQL

--- redis.sh
+++ redis.sh
@@ -1,8 +1,17 @@
-dnf install https://rpms.remirepo.net/enterprise/remi-release-8.rpm -y
-dnf module enable redis:remi-6.2 -y
-yum install redis -y
+PRINT "Install rpm package"
+dnf install https://rpms.remirepo.net/enterprise/remi-release-8.rpm -y &>>$LOG
 
-sed -i -e 's/127.0.0.1/0.0.0.0/' /etc/redis.conf /etc/redis/redis.conf
+PRINT "Enable redis"
+dnf module enable redis:remi-6.2 -y &>>$LOG
 
-systemctl enable redis
-systemctl start redis
+PRINT "INSTALL REDIS"
+yum install redis -y &>>$LOG
+
+PRINT "Configure redis listener"
+sed -i -e 's/127.0.0.1/0.0.0.0/' /etc/redis.conf /etc/redis/redis.conf &>>$LOG
+
+PRINT "Enable Redis"
+systemctl enable redis &>>$LOG
+
+PRINT "Restart Redis"
+systemctl restart redis &>>$LOG
