9af6f9148c668db668e308d1237c003749f0e07f
Sarbjeet <sarbjeetkaur310@gmail.com>
2022-12-30 17:55:41 -0500

    payment

--- payment.sh
+++ payment.sh
@@ -3,26 +3,3 @@ source Common.sh
 
 PYTHON
 
-
-
-
-useradd roboshop
-
-cd /home/roboshop
-curl -L -s -o /tmp/payment.zip "https://github.com/roboshop-devops-project/payment/archive/main.zip"
-unzip /tmp/payment.zip
-mv payment-main payment
-
-
-
-
-cd /home/roboshop/payment
-pip3 install -r requirements.txt
-
-
-    Update `CARTHOST` with cart server ip
-
-    Update `USERHOST` with user server ip
-
-    Update `AMQPHOST` with RabbitMQ server ip.
-
