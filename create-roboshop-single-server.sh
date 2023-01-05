5d2750a9e7aa286cf229a156ff9245a11e470cd7
Sarbjeet <sarbjeetkaur310@gmail.com>
2023-01-04 23:08:56 -0500

    demo

--- create-roboshop-single-server.sh
+++ create-roboshop-single-server.sh
@@ -40,8 +40,8 @@ if [ -z "$1" ]; then
   exit 1
 fi
 
-component=$1
+#component=$1
 #or component in catalogue cart user shipping payment frontend mongodb mysql rabbitmq redis dispatch; do
-  COMPONENT="${env}-${component}"
+  COMPONENT="${env}-${$1}"
   create_ec2
 #done
