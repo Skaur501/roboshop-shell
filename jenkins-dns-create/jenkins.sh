06c1bea761ab298d608839020a8e0826a4d36130
Sarbjeet <sarbjeetkaur310@gmail.com>
2023-01-15 00:23:52 -0500

    demo

--- jenkins-dns-create/jenkins.sh
+++ jenkins-dns-create/jenkins.sh
@@ -1,5 +1,5 @@
 # this file update the ip address in route53.jsoan (in jenkins)
-
+ZONE_ID="Z0863736QOU102FOZCW5"
 IP=$(aws ec2 describe-instances --filters "Name=tag:Name,Values=jenkins" | jq ".Reservations[]".Instances[].PublicIpAddress)
 sed -e "s/IPADDRESS/$(IP)/" route53.json >/tmp/record.json
 aws route53 change-resource-record-sets --hosted-zone-id ${ZONE_ID} --change-batch file:///tmp/record.json | jq
